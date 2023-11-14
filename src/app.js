3c85ab8e513fa5b67108ad557900363991d5e527
Pedro Henrique Bock <pedrohenriquecbock@gmail.com>
2023-11-13 23:24:03 -0300

    new: cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const taskrouter = require('./routes/tasks');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,8 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.use('/tasks', taskrouter);
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
