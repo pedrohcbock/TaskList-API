2564102742ccc07c1aadc18affe4af2eff8d840b
Pedro Henrique Bock <pedrohenriquecbock@gmail.com>
2023-11-20 16:18:23 -0300

    correções

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
+
 app.use(bodyParser.json());
 
 app.use('/tasks', taskrouter);
 
-app.use(cors());
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
